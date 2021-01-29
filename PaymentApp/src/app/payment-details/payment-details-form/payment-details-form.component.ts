619078059002af422c70cdc6df3fb66996dd4a37
Joshua Rose <Joshua.rose1@outlook.com>
2021-01-29 16:48:03 -0600

    point in time stop

---
+++ PaymentApp/src/app/payment-details/payment-details-form/payment-details-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { PaymentDetailService } from 'src/app/shared/payment-detail.service';
+
+@Component({
+  selector: 'app-payment-details-form',
+  templateUrl: './payment-details-form.component.html',
+  styleUrls: ['./payment-details-form.component.scss']
+})
+export class PaymentDetailsFormComponent implements OnInit {
+
+  constructor(public service:PaymentDetailService) { }
+
+  ngOnInit(): void {
+  }
+
+}
